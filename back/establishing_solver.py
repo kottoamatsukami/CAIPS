66842d10321b82a965880b9c361ea421b4eacd28
Alxmis <amishch.al@gmail.com>
2023-04-06 20:01:45 +0300

    establishing_solver

--- back/establishing_solver.py
+++ back/establishing_solver.py
@@ -1,15 +1,17 @@
 import pickle
 import numpy as np
 
+EPS = 0.0001
+TAU = 0.005
 class EstablishingSolver(object):
+
+    global EPS, TAU
 
     @staticmethod
     def load(path: str) -> dict:
         with open(path, "rb") as file:
             data = pickle.load(file)
         return data
-
-    # C = (3*np.pi)/8
 
     def get_system_values(self, values: float or int) -> float or int:
         # ----------------------------------------
@@ -27,7 +29,6 @@ class EstablishingSolver(object):
         # value_10 : C
         # }
         # ----------------------------------------
-        eps = 0.001
         F = np.empty([5, 1])
         F[0] = values[0] + values[2] * np.cos(3*np.pi/2 - values[3]) - values[5]
         F[1] = values[1] + values[2] * np.cos(3*np.pi/2 + values[4]) - values[7]
@@ -52,26 +53,19 @@ class EstablishingSolver(object):
         # value_10 : C
         # }
         # ----------------------------------------
-        X = [np.empty([5, 1]) for _ in range(2)]
-        eps = 0.01
-        tau = 0.005
+        X = [np.array([0.0, 0.0, 0.0, 0.0, 0.0]) for _ in range(2)]
 
         key = 0
-        while (key == 0 or np.linalg.norm(abs(X[0]-X[1]), ord=2) > eps):
-            X[0] = X[1]
+        while (key == 0 or np.linalg.norm(abs(X[0]-X[1]), ord=2) > EPS):
+            X[0] = X[1].copy()
             for i in range(5):
-                X[1][i] = X[1][i] - self.get_system_values(values)[i] * tau
+                X[1][i] = X[1][i] - self.get_system_values(values)[i] * TAU
+                values[i] = X[1][i]
             key = 1
         return X[1]
 
 
-vals = [4.0, 5.0, 3.0, 0.0, 0.0, -0.353, 0.3, 0.353, 0.3, 3*np.pi/8]
-res0 = EstablishingSolver().establish(vals)
-print(res0)
-res = res0
-res = np.append(res, -0.353)
-res = np.append(res, 0.3)
-res = np.append(res, 0.353)
-res = np.append(res, 0.3)
-res = np.append(res, 3*np.pi/8)
-print(EstablishingSolver().get_system_values(res))
+
+# # Add data from GUI here
+# vals = [20.0, 45.0, 30.0, 0.0, 0.0, -0.353, 0.3, 0.353, 0.3, 3*np.pi/8]
+# values = EstablishingSolver().establish(vals)
