13e2ea3bfd80189663077737575fc7ffa8f6ad9f
Enterprice <om1cr0ne@yandex.ru>
2023-04-08 20:00:57 +0300

    Node system out of beta

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,7 +1,29 @@
+import math  # Working with the math library
+
+
 STD_SETTINGS = {
     "width": 1200,
     "height": 800,
     "min_width": 1200,
     "min_height": 800,
     "title": "CAIPS - Simulation",
+    "small_icon_path": "front/icons/wing_small.ico",
+    "large_icon_path": "front/icons/wing_large.ico",
+}
+
+STD_PARAMETERS = {
+            "Ax": 0,
+            "Ay": 0.95 * 2,
+            "Bx": 0.30 * 2,
+            "By": 0.65 * 2,
+            "Rt": 0.30 * 2,
+            "Rb": 0.19 * 2,
+            "Pt0": 12000 * 2,
+            "Pb0": 4000 * 2,
+            "Pac": 2000,
+            "Xtop": 0,
+            "Ytop": 0.65 * 2,
+            "Xbot": 0,
+            "Ybot": 0.22 * 2,
+            "alpha5": 3*math.pi/2,
 }
