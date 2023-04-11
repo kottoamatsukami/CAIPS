f6c52d17b9e59e8a24fcba19cc23632a21fc5d77
Alxmis <amishch.al@gmail.com>
2023-04-11 23:55:45 +0300

    added tracking_dynamics.py (currently at the development stage)

--- back/tracking_dynamics.py
+++ back/tracking_dynamics.py
@@ -1,5 +1,4 @@
 import establishing_solver
-import Animation
 import numpy as np
 import matplotlib.patches as mpatches
 
@@ -14,18 +13,25 @@ class DynamicsSolver(object):
 
     @staticmethod
     def Dvdt(l: float or int) -> float or int:
-        return 1/M * (P * l - M * G)
-
-
-    def calculate_y(self, values: float or int) -> float or int:
         # -----------------------------------
         # structure
         # values = {
-        # value_1 : x1_0 = 0
-        # value_2 : x2_0 = 0
-        # value_3 : y_0 = Ay = By
-        # value_4 : phi1_0 = 0
-        # value_5 : phi2_0 = 0
+        # value_1 : x1
+        # value_2 : x2
+        # }
+        # -----------------------------------
+        return 1/M * (P * l - M * G)
+
+
+    def CalculateY(self, values: float or int) -> float or int:
+        # -----------------------------------
+        # structure
+        # values = {
+        # value_1 : x1 = Ax
+        # value_2 : x2 = Bx
+        # value_3 : y = Ay = By
+        # value_4 : phi1 = 0
+        # value_5 : phi2 = 0
         # value_6 : Ax = from user
         # value_7 : Ay = from user
         # value_8 : Bx = from user
@@ -35,32 +41,24 @@ class DynamicsSolver(object):
         # }
         # -----------------------------------
 
-        # ADD VALUES TO Y_ARRAY
-        data = [[0 for i in range(9)] for _ in range(250)]
+        Y = []
 
-        for t_ in range(250):
-            for j in range(len(data[0])):
-                data[t_][j] = values[j]
-            values[6] = values[6] + values[10] * DTIME
-            values[8] = values[6]
+        for t_ in range(0, 250, 1):
             X = establishing_solver.EstablishingSolver().establish(values)
-            for i in range(3):
+            for i in range(len(X)):
                 values[i] = X[i]
-            l = abs(values[1] - values[0])
+            l = values[1] - values[0]
             values[10] = values[10] + self.Dvdt(l) * DTIME
+            values[5] = values[0]
+            values[6] = values[6] + values[10] * DTIME
+            values[7] = values[1]
+            values[8] = values[6]
 
-            # values[5] = values[0]
-            # values[7] = values[1]
+            print("t_ = ", t_)
 
-            print("t_ =", t_, "l =", l, "values[1] =", values[1], "values[0] =", values[0])
-            # if t_ == 41:
-            #     print(values)
-            #     input()
-                # print("\n\n\n\n\n\n\n\n\n")
 
-        return data
+        return values
 
-vals = [0.0, 0.0, 0.3, 0.0, 0.0, -0.353, 0.3, 0.353, 0.3, 3*np.pi/8, 0]
-data = DynamicsSolver().calculate_y(vals)
-print(data)
-Animation.Animation().create_animation(data)
+vals = [0.0, 0.0, 0.0, 0.0, 0.0, -0.353, 0.3, 0.353, 0.3, 3*np.pi/8, 0]
+values = DynamicsSolver().CalculateY(vals)
+print(values)
