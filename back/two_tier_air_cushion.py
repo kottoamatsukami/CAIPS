9729dc85305943ff918c5623567ffe58c6a7cb2e
Enterprice <om1cr0ne@yandex.ru>
2023-04-25 21:02:32 +0300

    halo 3

--- back/two_tier_air_cushion.py
+++ back/two_tier_air_cushion.py
@@ -5,137 +5,100 @@ import back
 
 
 class TwoTierSolver(object):
-    def __init__(self, constants: dict) -> None:
-        self.constants = constants
+    def __init__(self, const: list) -> None:
+        """
+        0 -> Ax  4 -> phiNd1  7 ->  p  10 -> alpha5
+        1 -> Ay  5 -> phiNd2  8 -> rt
+        2 -> Bx  6 -> phiNd3  9 -> rb
+        3 -> By
+        """
+        self.constants = const
 
-    def F(self, x: dict, norm=1, return_f=False) -> np.array:
-        f = np.zeros(43)
-        f[0] = 5.001905970 - x["phiNd4"] - x["phiNd5"]
-        f[1] = 3*np.pi/2 - x["phi4"] - x["alpha4"]
-        f[2] = x["r1"] * x["phi1"] - self.constants["rt"] * self.constants["phiNd1"]
-        f[3] = x["r2"] * x["phi2"] - self.constants["rt"] * self.constants["phiNd2"]
-        f[4] = x["r3"] * x["phi3"] - self.constants["rt"] * self.constants["phiNd3"]
-        f[5] = x["r4"] * x["phi4"] + x["r5"] * x["phi5"] - self.constants["rb"]*(x["phiNd4"] + x["phiNd5"])
-        f[6] = x["r1"] * np.cos(x["alpha1"]) + x["x1"] - self.constants["Ax"]
-        f[7] = x["r1"] * np.sin(x["alpha1"]) + x["y1"] - self.constants["Ay"]
-        f[8] = x["r2"] * np.cos(x["phi2"] + x["alpha2"]) + x["x2"] - self.constants["Bx"]
-        f[9] = x["r2"] * np.sin(x["phi2"] + x["alpha2"]) + x["y2"] - self.constants["By"]
-        f[10] = x["r3"] * np.cos(x["alpha3"] + x["phi3"]) + x["x3"] - x["r2"]*np.cos(x["alpha2"]) - x["x2"]
-        f[11] = x["r3"] * np.sin(x["alpha3"] + x["phi3"]) + x["y3"] - x["r2"]*np.sin(x["alpha2"]) - x["y2"]
-        f[12] = x["r3"] * np.cos(x["alpha3"] + x["phi3"]) + x["x3"] - x["r5"]*np.cos(self.constants["alpha5"] + x["phi5"]) - x["x5"]
-        f[13] = x["r3"] * np.sin(x["alpha3"] + x["phi3"]) + x["y3"] - x["r5"]*np.sin(self.constants["alpha5"] + x["phi5"]) - x["y5"]
-        f[14] = x["r1"] * np.cos(x["alpha1"] + x["phi1"]) + x["x1"] - x["r3"]*np.cos(x["alpha3"]) - x["x3"]
-        f[15] = x["r1"] * np.sin(x["alpha1"] + x["phi1"]) + x["y1"] - x["r3"]*np.sin(x["alpha3"]) - x["y3"]
-        f[16] = x["r1"] * np.cos(x["alpha1"] + x["phi1"]) + x["x1"] - x["r4"]*np.cos(x["alpha4"]) - x["x4"]
-        f[17] = x["r1"] * np.sin(x["alpha1"] + x["phi1"]) + x["y1"] - x["r4"]*np.sin(x["alpha4"]) - x["y4"]
-        f[18] = x["x4"] - x["x5"]
-        f[19] = x["y4"] - x["r4"] - x["y5"] + x["r5"]
-        f[20] = x["pt"]*x["r1"]*np.sin(x["alpha1"] + x["phi1"]) - (x["pt"] - x["pb"])*x["r3"]*np.sin(x["alpha3"]) - x["pb"]*x["r4"]*np.sin(x["alpha4"])
-        f[21] = x["pt"]*x["r1"]*np.cos(x["alpha1"] + x["phi1"]) - (x["pt"] - x["pb"])*x["r3"]*np.cos(x["alpha3"]) - x["pb"]*x["r4"]*np.cos(x["alpha4"])
-        f[22] = (x["pt"] - self.constants["p"])*x["r2"]*np.sin(x["alpha2"]) - (x["pt"] - x["pb"])*x["r3"]*np.sin(x["alpha3"] + x["phi3"]) - (x["pb"] - self.constants["p"])*x["r5"]*np.cos(self.constants["alpha5"] + x["phi5"])
-        f[23] = (x["pt"] - self.constants["p"])*x["r2"]*np.cos(x["alpha2"]) - (x["pt"] - x["pb"])*x["r3"]*np.cos(x["alpha3"] + x["phi3"]) - (x["pb"] - self.constants["p"])*x["r5"]*np.cos(self.constants["alpha5"] + x["phi5"])
-        f[24] = x["pb"]*x["r4"] - (x["pb"] - self.constants["p"])*x["r5"]
+    def F(self, x: list, norm=1, return_f=False) -> np.array:
+        """
+        0 -> x1  5 -> y1  10 -> r1  15 -> phi1  20 -> alpha1  24 -> phiNd4
+        1 -> x2  6 -> y2  11 -> r2  16 -> phi2  21 -> alpha2  25 -> phiNd5
+        2 -> x3  7 -> y3  12 -> r3  17 -> phi3  22 -> alpha3  EXTRA
+        3 -> x4  8 -> y4  13 -> r4  18 -> phi4  23 -> alpha4  26 -> pt
+        4 -> x5  9 -> y5  14 -> r5  19 -> phi5                27 -> pb
+        """
+        func = np.zeros(25)
 
-        for i in range(len(f)):
-            f[i] = f[i] * f[i]
+        func[0] = 5.001905970 - x[24] - x[25]
+        func[1] = x[4] - x[3]
+        func[2] = x[23] - 3*np.pi/2 + x[18]
 
+        func[3] = x[10] * x[15] - self.constants[8] * self.constants[4]
+        func[4] = x[11] * x[16] - self.constants[8] * self.constants[5]
+        func[5] = x[12] * x[17] - self.constants[8] * self.constants[6]
+        func[6] = x[13] * x[18] + x[14] * x[19] - self.constants[9] * (x[24] + x[25])
+
+        func[7] = x[10] * np.cos(x[20]) + x[0] - self.constants[0]
+        func[8] = x[10] * np.sin(x[20]) + x[5] - self.constants[1]
+
+        func[8] = x[11] * np.cos(x[16] + x[21]) + x[1] - self.constants[2]
+        func[9] = x[11] * np.sin(x[16] + x[21]) + x[6] - self.constants[3]
+
+        func[10] = x[12] * np.cos(x[22] + x[17]) + x[2] - x[11]*np.cos(x[21]) - x[1]
+        func[11] = x[12] * np.sin(x[22] + x[17]) + x[7] - x[11]*np.sin(x[21]) - x[6]
+        func[12] = x[12] * np.cos(x[22] + x[17]) + x[2] - x[14]*np.cos(self.constants[10] + x[19]) - x[4]
+        func[13] = x[12] * np.sin(x[22] + x[17]) + x[7] - x[14]*np.sin(self.constants[10] + x[19]) - x[9]
+
+        func[14] = x[10] * np.cos(x[20] + x[15]) + x[0] - x[12]*np.cos(x[22]) - x[2]
+        func[15] = x[10] * np.sin(x[20] + x[15]) + x[5] - x[12]*np.sin(x[22]) - x[7]
+        func[16] = x[10] * np.cos(x[20] + x[15]) + x[0] - x[13]*np.cos(x[23]) - x[3]
+        func[17] = x[10] * np.sin(x[20] + x[15]) + x[5] - x[13]*np.sin(x[23]) - x[8]
+
+        func[18] = x[8] - x[13] - x[9] + x[14]
+
+        func[20] = x[26]*x[10]*np.sin(x[20] + x[15]) - (x[26] - x[27])*x[12]*np.sin(x[22]) - x[27]*x[13]*np.sin(x[23])
+        func[21] = x[26]*x[10]*np.cos(x[20] + x[15]) - (x[26] - x[27])*x[12]*np.cos(x[22]) - x[27]*x[13]*np.cos(x[23])
+
+        func[22] = (x[26] - self.constants[7])*x[11]*np.sin(x[21]) - (x[26] - x[27])*x[12]*np.sin(x[22] + x[17]) - (x[27] - self.constants[7])*x[14]*np.sin(self.constants[10] + x[19])
+        func[23] = (x[26] - self.constants[7])*x[11]*np.cos(x[21]) - (x[26] - x[27])*x[12]*np.cos(x[22] + x[17]) - (x[27] - self.constants[7])*x[14]*np.cos(self.constants[10] + x[19])
+        func[24] = x[27]*x[13] - (x[27] - self.constants[7])*x[14]
+
+        func = list(map(lambda v: v**2, func))
+        func = np.array(func)
         if return_f:
-            return np.linalg.norm(f, norm), f
-        return np.linalg.norm(f, norm)
+            return np.linalg.norm(func, norm), func
+        return np.linalg.norm(func, norm)
 
-    def findSolution(self, vector, norm=2):
-        for i in range(1, 5000):
-            gradient = {parameter: 0 for parameter in vector}
-            for parameter in vector:
-                epsvector = vector.copy()
-                epsvector[parameter] += back.EPS
-                gradient[parameter] = (self.F(epsvector, norm) - self.F(vector, norm))/back.EPS
-
-            for parameter in vector:
-                vector[parameter] = vector[parameter] - back.OMI * gradient[parameter]
-
-            print(self.F(vector, norm))
+    def findSolution(self, vec, norm=2):
+        for _ in range(1, 500):
+            gradient = np.zeros_like(vec)
+            for j in range(len(vec)):
+                epsilon = np.zeros_like(vec)
+                epsilon[j] = back.EPS
+                gradient[j] = (self.F(vec + epsilon) - self.F(vec - epsilon)) / (2 * back.EPS)
+            vec -= back.OMI * gradient
+            norma = self.F(vec, norm=norm)
+            print(norma)
         return vector
 
 
-    def makeAnimation(self, vector):
-        ...
 
-    def makePlot(self, vector, rotate=False):
-        # init PLT
-        fig, axs = plt.subplots(figsize=(5, 5))
-        axs.grid(linestyle='--')
-        axs.set_aspect('equal')
-
-        # Line AB
-        axs.plot(
-            (self.parameters["Ax"], self.parameters["Bx"]),
-            (self.parameters["Ay"], self.parameters["By"]),
-            color="black"
-        )
-
-        # Convert to GIF format
-        fig.savefig("saved_parameters/temp.png")
-        im = Image.open("saved_parameters/temp.png")
-        if rotate:
-            im = im.rotate(180)
-        im.save("saved_parameters/temp.gif")
+constants = np.array([
+    0,            # Ax
+    1.9,          # Ay
+    0.6,          # Bx
+    1.3,          # By
+    2.753364902,  # phiNd1
+    1.182568575,  # phiNd2
+    0.776455503,  # phiNd3
+    2000,         # p
+    0.6,          # rt
+    0.38,         # rb
+    3*np.pi/2,    # alpha5
+])
+vector = np.ones(28)
 
 
+solver = TwoTierSolver(const=constants)
+vector = solver.findSolution(vec=vector, norm=2)
 
-    def calculate_overpressure(self, n):
-        ...
+f = solver.F(vector, return_f=True)
 
+for i in range(len(f[1])):
+    print(f"Уравнение {i}: {round(f[1][i], 3)}")
 
-constants = {
-    "p": 2000,
-    "Ax": 0,
-    "Ay": 1.9,
-    "Bx": 0.6,
-    "By": 1.3,
-    "xTop": 0,
-    "yTop": 1.3,
-    "xBot": 0,
-    "yBot": 0.44,
-    "rt": 0.6,
-    "rb": 0.38,
-    "pt0": 24000,
-    "pb0": 8000,
-    "phiNd1": 2.753364902,
-    "phiNd2": 1.182568575,
-    "phiNd3": 0.776455503,
-    "alpha5": 3*np.pi/2,
-}
-vector = {
-    "phiNd4": 0.5,
-    "phiNd5": 0.5,
-    "x1": 1,
-    "x2": 1,
-    "x3": 1,
-    "x4": 1,
-    "x5": 1,
-    "y1": 1,
-    "y2": 1,
-    "y3": 1,
-    "y4": 1,
-    "y5": 1,
-    "alpha1": 0.5,
-    "alpha2": 0.5,
-    "alpha3": 0.5,
-    "alpha4": 0.5,
-    "phi1": 0.5,
-    "phi2": 0.5,
-    "phi3": 0.5,
-    "phi4": 0.5,
-    "phi5": 0.5,
-    "r1": 2,
-    "r2": 2,
-    "r3": 2,
-    "r4": 2,
-    "r5": 2,
-    "pt": 1,
-    "pb": 1,
-}
-solver = TwoTierSolver(constants=constants)
-vector = solver.findSolution(vector=vector)
-print(solver.F(vector), vector)
